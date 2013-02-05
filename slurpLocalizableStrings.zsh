d7a18ec967897b771871038f1601d2466d8c3c4f
philippm <philipp@incrediblebee.com>
2013-02-05 23:00:08 +0100

    Allow overriding genstrings input folders
    After the path to xibLocalizationPostprocessor any (optional)
    remaining arguments are treated as input folders to genstrings.
    
    Previously this was fixed at ${SRCROOT} but this could be slow if
    the project contains lots of source files, even if they aren't members
    of any target.

--- slurpLocalizableStrings.zsh
+++ slurpLocalizableStrings.zsh
@@ -5,20 +5,30 @@
 # environment variable.
 #
 # This script takes an optional argument that lets you override this default.
+#
+# Any remaining arguments are treated as input folders for genstrings; if none
+# are given this defaults to $SRCROOT.
 
 # See if the user provided a path to the XIB localization post processor script
 # otherwise fall back on the build directory
 if (( ${+1} )); then
   XIB_PROCESSOR_PATH=$1
+  shift
 else
   XIB_PROCESSOR_PATH=${BUILT_PRODUCTS_DIR}/xibLocalizationPostprocessor
+fi
+
+# Any remaining arguments are treated as folders in which to run genstrings
+# (default is $SRCROOT)
+if ! (( ${+1} )); then
+    1=${SRCROOT}
 fi
 
 # Internationalize source code -- piped through xargs to support a large number
 # of input files
 OUTPUT_FOLDER="${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/${DEVELOPMENT_LANGUAGE}.lproj"
 # -q silences duplicate comments with same key warning
-find ${SRCROOT} -name "*.[hm]" -print0 | xargs -0 genstrings -q -a -o "${OUTPUT_FOLDER}" 
+find "$@" -name "*.[hm]" -print0 | xargs -0 genstrings -q -a -o "${OUTPUT_FOLDER}" 
 
 # Internationalize XIBs
 foreach nibFile (${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/**/*.nib)
